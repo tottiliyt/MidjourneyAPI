1a0cfac52df82c9d188f7024d54854530cfa698f
tottiliyt <tottiliyt@gmail.com>
2023-04-29 17:28:40 -0700

    add button, rate limiter, jwt token

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,8 @@
-# a place to write random things, need to be removed eventually
-
 from discum.utils.slash import SlashCommander
+from discum.utils.button import Buttoner
 import discum
 import json
+import requests
 
 # To obtain your token, https://discordpy-self.readthedocs.io/en/latest/token.html
 TOKEN= 'NTUyNzI2NTQ2ODIyMDcwMjgy.YdPFng.uUxKSWpVFtSdVfVM-cUkgnogxCk'
@@ -13,39 +13,121 @@ WEBHOOK_BOT_ID = "1099816041737101462"
 MIDJOURNEY_BOT_ID = "936929561302675456"
 IMAGINE_PREFIX = "@IMAGINE"
 
-def imagine(resp):
-  if resp.event.message:
-    msg = resp.parsed.auto()
+current_job = {}
 
-    # check owner send message
+TEST_WEBHOOK_URL = "https://webhook.site/4ab0ea73-7086-434d-9797-dd47f87009b2"
+LOCALHOST_WEBHOOK_URL = "http://127.0.0.1:5000/webhook"
+
+def endpoints(resp):
+  if resp.event.message or resp.event.message_updated:
+    msg = resp.parsed.auto()
+    # Listening to owner sent message
     if msg['author']['id'] == WEBHOOK_BOT_ID:
         content_string = msg['content']
-
         try:
             content_json = json.loads(content_string)
             cmd = content_json["cmd"]
-            prompt = content_json["msg"]
-            webhook = content_json["webhook"]
         except:
-            # not able to parse request body
+            # Not able to parse request body
             return
 
         if cmd == "imagine":
-            channelID = msg['channel_id']
-            guildID = msg['guild_id']
+            try:
+                prompt = content_json["msg"]
+                webhook_id = content_json["webhook_id"]
+            except:
+                # Not able to parse request body
+                return
+            try:
+                channelID = msg['channel_id']
+                guildID = msg['guild_id']
+                slashCmds = bot.getSlashCommands(MIDJOURNEY_BOT_ID).json()
+                slashCmd = SlashCommander(slashCmds)
+                metadata = slashCmd.get(['imagine'])
+            except:
+                # Not able to fetch neccessary info to send the slash command
+                return
 
-            bot.sendMessage(channelID,f"got request: /imagine {prompt}")
-            slashCmds = bot.getSlashCommands(MIDJOURNEY_BOT_ID).json()
-            slashCmd = SlashCommander(slashCmds)
-            metadata = slashCmd.get(['imagine'])
-
-            # modify slash command metadata 
+            # Modify slash command metadata 
             metadata['options'] = [{
                 "type": 3,
                 "name": "prompt",
                 "value": prompt
             }]
+
             bot.triggerSlashCommand(MIDJOURNEY_BOT_ID, channelID,guildID=guildID, data=metadata)
 
-bot.gateway.command({"function": imagine})
+            # add to current job
+            if prompt not in current_job:
+                current_job[prompt] = []
+            current_job[prompt].append(webhook_id)
+
+        elif cmd == "button":
+            try:
+                metadata = content_json['metadata']
+                button_name = content_json['button_name']
+                webhook_id = content_json["user_id"]
+                prompt: content_json['prompt']
+
+            except:
+                # Not able to parse request body
+                return
+            buts = Buttoner(metadata["components"])
+            bot.click(
+                MIDJOURNEY_BOT_ID,
+                channelID=metadata["channel_id"],
+                guildID=metadata["guild_id"],
+                messageID=metadata["id"],
+                messageFlags=metadata["flags"],
+                data=buts.getButton(button_name),
+            )
+            
+    # Listening to midjourney bot sent message
+    elif msg['author']['id'] == MIDJOURNEY_BOT_ID:
+        prompt_identifier = "**"
+        content = msg['content']
+
+        prompt = content[content.find(prompt_identifier)+len(prompt_identifier):content.rfind(prompt_identifier)]
+        
+        print(current_job)
+        # determine which webhook to send
+        webhook_id = current_job[prompt][0]
+
+        # job wait to start
+        if len(msg['attachments']) == 0:
+            result_metadata = {
+                "msg": msg["content"],
+                "webhook_id": webhook_id
+            }
+            response = requests.post(LOCALHOST_WEBHOOK_URL, json=result_metadata)
+        # job started or finished
+        elif len(msg['attachments']) == 1:
+            
+            #job in progress
+            if len(msg['components']) == 0:
+                result_metadata = {
+                    "msg": msg["content"],
+                    "webhook_id": webhook_id
+                }
+                response = requests.post(LOCALHOST_WEBHOOK_URL, json=result_metadata)
+
+            #job finished
+            else:
+                try:
+                    result_metadata = {
+                        'channel_id':msg["channel_id"],
+                        'guild_id':msg["guild_id"],
+                        'id':msg["id"],
+                        'flags':msg["flags"],
+                        'components':msg['components'],
+                        'attachments':msg['attachments'],
+                        'prompt': prompt
+                    }
+                
+                except:
+                    # Not able to parse request body
+                    return
+                response = requests.post(LOCALHOST_WEBHOOK_URL, json=result_metadata)
+
+bot.gateway.command({"function": endpoints})
 bot.gateway.run(auto_reconnect=True)
